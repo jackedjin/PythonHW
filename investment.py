e7c4daf5b973c7462c448ab1cb7c94d6d39ddddc
Elia Lerma <elerma@atlassian.com>
2021-03-02 20:30:43 -0800

    l

--- investment.py
+++ investment.py
@@ -7,13 +7,13 @@ def calculate_apr(principal,interest_rate,years):
 	principal=500
 	interest_rate=0.03
 	years=65
-	x=1
+	x=0
 #	print(f'The new principal after 1 year is {principal*(1+interest_rate)}')
 
 	if interest_rate<0.00:
 		print("False")
 		exit()
-	for x in range(1,years):
+	for x in range(0,years):
 		principal=principal*(1+interest_rate)
 		print(f'After year {x}, the new principal is {principal}')
 		x+=1
