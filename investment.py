accd7aed15a1dcddab4eb3e1b6c2d77cd4d33332
Elia Lerma <elerma@atlassian.com>
2021-03-03 11:26:58 -0800

    now

--- investment.py
+++ investment.py
@@ -6,8 +6,9 @@ def calculate_apr(principal,interest_rate,years):
 	and it checks whether the interest rate is a negative value with an if-statement.
 	Then, it calculates the principal after each year based on a compound interest.
 	"""
+	x='false'
 	if interest_rate<0:
-		return str(false)
+		return(x)
 		exit()
 
 	principal=500
