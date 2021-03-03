79580a172ffe261d1afddfe4fb2576e955763343
Elia Lerma <elerma@atlassian.com>
2021-03-03 00:22:30 -0800

    wth

--- timing.py
+++ timing.py
@@ -3,7 +3,7 @@ def calculate_time(func):
 	def wrapper_calculatetime():
 		start = time.time()
 		func()
-		time.sleep(2)
+		time.sleep(3)
 		stop = time.time()
 		X=stop-start
 		print(f'Total time {X}')
