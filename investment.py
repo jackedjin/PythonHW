548f8a758a03d3da54d64649c0fe6ab2d7a22927
Elia Lerma <elerma@atlassian.com>
2021-03-02 17:22:44 -0800

    coded investment.py commit

---
+++ investment.py
@@ -0,0 +1,11 @@
+def calculator():
+	principal=500
+	interet_rate=0.03
+	years=65
+
+	for x in range(years)
+		principal=principal*(1+interest_rate)
+		print(f'After year {x}, the new principal is {principal}')
+		x+=1
+		print(f'On the {x}th year, the principal has become {principal}')
+calculate_apr()
