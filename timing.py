54c1234634aaa77a5115e54c13ff7c00c5ffed0b
Elia Lerma <elerma@atlassian.com>
2021-03-02 23:56:34 -0800

    again

--- timing.py
+++ timing.py
@@ -1,10 +1,9 @@
-import functools
 import time
 def calculate_time(func):
-	@functools.wraps(func)
-	def wrapper_calculate_time(*args, **kwargs):
+#	@functools.wraps(func)
+	def wrapper_calculate_time():
 		start_time = time.perf_count()
-		value = func(*args, **kwargs)
+		value = func()
 		end_time = time.perf_count()
 		run_time = end_time - start_time
 		print(f'Total time taken to complete function is {run_time} seconds')
