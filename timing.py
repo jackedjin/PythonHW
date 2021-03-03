1992e900fb5f45690243e125c6a2154b79adc5ba
Elia Lerma <elerma@atlassian.com>
2021-03-03 00:19:33 -0800

    please

--- timing.py
+++ timing.py
@@ -1,9 +1,10 @@
 import time
 def calculate_time(func):
-	def wrapper_calculate_time():
+	def wrapper_calculatetime():
 		start = time.time()
 		func()
 		time.sleep(2)
 		stop = time.time()
-		print(f'Total time taken is {stop-start} seconds')
-	return wrapper_calculate_time
+		X=stop-start
+		print(f'Total time taken is {X} seconds')
+	return wrapper_calculatetime
