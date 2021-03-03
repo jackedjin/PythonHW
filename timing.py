0d15bed311509285b92f014fef1d0421aa5ca4d0
Elia Lerma <elerma@atlassian.com>
2021-03-02 23:53:55 -0800

    correct?

--- timing.py
+++ timing.py
@@ -1,7 +1,12 @@
+import functools
 import time
-def calculate_time():
-	print(f'Total time {time.time()}')
-	print(f'{time.sleep(2)}')
-calculate_time()
-#def time_time():
-
+def calculate_time(func):
+	@functools.wraps(func)
+	def wrapper_calculate_time(*args, **kwargs):
+		start_time = time.perf_count()
+		value = func(*args, **kwargs)
+		end_time = time.perf_count()
+		run_time = end_time - start_time
+		print(f'Total time taken to complete function is {run_time} seconds')
+		return value
+	return wrapper_calculate_time
