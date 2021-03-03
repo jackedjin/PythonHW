0b1cb10ac8e09dc36571aa5efd40d49521403105
Elia Lerma <elerma@atlassian.com>
2021-03-02 17:44:23 -0800

    new corrected investment.py

--- investment.py
+++ investment.py
@@ -1,10 +1,17 @@
 def calculate_apr():
-	principal=500
-	interest_rate=0.03
-	years=65
-
+	principal=input("Enter principal:")
+	principal=int(principal)
+	print(principal)
+	interest_rate=input("Enter interest rate:")
+	interest_rate=int(interest_rate)
 	if interest_rate<0:
-		print("false")
+		print("False")
+		exit()
+	else:
+		print(interest_rate)
+	years=input("Enter number of years:")
+	years=int(years)
+	print(years)
 
 	for x in range(years):
 		principal=principal*(1+interest_rate)
