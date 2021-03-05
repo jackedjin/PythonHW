77355782da612832f4eb4cc3078336ff13f7b328
Elia Lerma <elerma@atlassian.com>
2021-03-04 18:20:48 -0800

    ffs

--- investment.py
+++ investment.py
@@ -8,10 +8,8 @@ def calculate_apr(principal,interest_rate,years):
 	"""
 
 	if principal<0 or interest_rate<=0 or years<0:
-#or principal<0 or years<0:
+		return "false"
 		exit()
-		return false
-
 	else:
 #		principal=principal*(1+interest_rate)
 		x=0
