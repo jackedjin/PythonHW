6e371b872fdcf04c504bbf0684e3fd702462b505
Elia Lerma <elerma@atlassian.com>
2021-03-04 18:54:52 -0800

    finalized investment.py

--- investment.py
+++ investment.py
@@ -7,15 +7,14 @@ def calculate_apr(principal,interest_rate,years):
 	Then, it calculates the principal after each year based on a compound interest.
 	"""
 
-	if principal<0 or interest_rate<=0 or years<0:
+	if principal<0 or interest_rate<=0 or years<0: #checks if any of the parameters are a negative value which immediately returns False and exits
 		return False
 		exit()
 	else:
-#		principal=principal*(1+interest_rate)
-		x=0
-		while x<years:
+		x=0 #initializing of x
+		while x<years: #while loop runs until the last year
 			principal=principal*(1+interest_rate)
-			x+=1
+			x+=1 #counter which automatically adds 1 and keeps the loop running
 			new_principal = float("{:.7f}".format(principal))
 
 	return new_principal
