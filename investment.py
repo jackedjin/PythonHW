7fbeb37740e10e67595c0873025631a569d74200
Elia Lerma <elerma@atlassian.com>
2021-03-04 17:16:13 -0800

    changed

--- investment.py
+++ investment.py
@@ -6,13 +6,21 @@ def calculate_apr(principal,interest_rate,years):
 	and it checks whether the interest rate is a negative value with an if-statement.
 	Then, it calculates the principal after each year based on a compound interest.
 	"""
-	principal=500
-	interest_rate=0.03
+	principal=100
+	interest_rate=0.06
 	years=65
-	x=0
-	while x<years:
-		principal=principal*(1+interest_rate/100)
-#		print(f'After year {years}, the new principal is {principal}')
-		x+=1
-		new_principal = float("{:.7f}".format(principal))
-	return new_principal
+
+	one_year=principal*(1+interest_rate)
+	if interest_rate<0:
+		return false
+		exit()
+	else:
+		x=0
+		while x<years:
+			principal=principal*(1+interest_rate/100)
+#			print(f'After year {years}, the new principal is {principal}')
+			x+=1
+			new_principal = float("{:.7f}".format(principal))
+	return_list=[oneyear,new_principal]
+
+	return return_list
